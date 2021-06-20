a5208282b69fe182377c0ec35e2e3a14c401085e
Ingvildb <79170628+Ingvildb@users.noreply.github.com>
2021-06-20 13:28:50 +0200

    final

---
+++ js/script.js
@@ -0,0 +1,135 @@
+//const cats = [
+   // {
+   //     name: "Blob",
+   //     age: 10
+    //},
+  //  {
+      //  name: "Harold",
+   // },
+    //{
+     //   name: "Blurt",
+       // age: 21
+    //}
+//];
+
+
+// Question 1 
+//const cat = {
+    //complain: function() {
+    //    console.log("Meow!");
+  //  }
+//}
+
+//cat.complain();
+
+
+// Question 2
+
+//const thirdHeading = document.querySelector("h3");
+//thirdHeading.innerHTML = "Updated heading";
+
+
+// Question 3
+
+
+//thirdHeading.style.fontSize = "2em";
+
+
+// Question 4
+
+//thirdHeading.innerHTML += "<div class=subheading></div>";
+
+
+// Question 5 
+
+
+//const paragraphs = document.querySelectorAll("p");
+//for (i = 0; i < paragraphs.length; i++) {
+  //  console.log(paragraphs)
+  //  paragraphs[i].style.color = "red";
+//}
+
+
+
+
+
+
+// Question 6
+
+//const resultsContainer = document.querySelector(".results");
+//console.log(resultsContainer)
+
+//resultsContainer.innerHTML = `<p>New Paragraph</p>`;
+//resultsContainer.style.backgroundColor = "red";
+ 
+
+
+
+
+// Question 7
+
+
+//const cats = [
+ //  {
+ //       name: "Blob",
+  //      age: 10
+ //   },
+//    {
+//       name: "Harold",
+ //   },
+ //   {
+ //       name: "Blurt",
+  //      age: 21
+  //  }
+//];
+
+//function printListItems(list) {
+
+  //  for(let i = 0; i < list.length; i++) {
+  //      console.log(list[i]);
+  //  }
+//}
+
+//printListItems(cats);
+
+
+// Question 8
+
+
+//const cats = [
+  //  {
+  //       name: "Blob",
+  //       age: 10
+  //   },
+ //    {
+  //       name: "Harold",
+  //   },
+   //  {
+     //    name: "Blurt",
+       //  age: 21
+    // }
+// ];
+ 
+
+// function createCats(cats) {
+
+   // for(let i =0; i < cats.length;i++){
+        
+     //   if(cats[i].age > 0) {
+       //     return ("Missing age")
+        //}
+
+        //const div = "<div>" + div + "</div>";
+        //return div;
+
+        //const h5 = div + "<h5>" + "name" + "</h5>" + div;
+       // return h5
+
+       // const newParagraph = div + "<h5>" + "age" + "</h5>" + div;
+
+   // }
+
+//}
+
+// createCats(cats);
+
